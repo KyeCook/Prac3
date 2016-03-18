9fd39a4712f90be7ab6462175e14977f3aa31bdf
KyeCook <acfanatics@hotmail.com>
2016-03-18 11:50:51 +1000

    Added Code
    Code not finalised or in working condition.. unsure of how to stop
    infinite looping

--- fillInTheBlanks.py
+++ fillInTheBlanks.py
@@ -1,9 +1,9 @@
-finished  =  False
-result  =  0
-while  not  finished:
-try:
-_______
-_______
-except  :
-print("Please  enter  a  valid  integer.")
-print("valid  result  is",  result)
+finished = False
+result = 0
+while not finished:
+    try:
+        result = input(int("Enter a number"))
+        finished = True
+    except ValueError:
+        print("Please  enter  a  valid  integer.")
+print("valid  result  is", result)
