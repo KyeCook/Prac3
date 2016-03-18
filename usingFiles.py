55814622b5298e4fe7eb662ac2b4ec72ecd5825a
KyeCook <acfanatics@hotmail.com>
2016-03-18 12:28:42 +1000

    Completed Basics for File Modification
    Finished Base question regarding file manipulation using python
    programming

---
+++ usingFiles.py
@@ -0,0 +1,31 @@
+"""
+Using files with programming
+writing and reading textfiles
+"""
+
+# -------------- Task 1 -- Writing ------------------------
+
+# user_name = input("Enter name:")
+# file = open('name.txt', 'w')
+# file.write(user_name + '\n')
+# file.close()
+
+# --------------- Task 2 -- Reading/printing  -------------
+
+# file = open('name.txt', 'r')
+# names = file.read()
+# print("Your name is", names)
+
+# --------------- Task 3 -- Reading/adding ----------------
+
+file = open("numbers.txt", 'r')
+lines = file.readlines()
+number_1 = lines[0]
+number_2 = lines[1]
+
+total = int(number_1) + int(number_2)
+print(total)
+
+
+
+
