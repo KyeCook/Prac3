be5a5ff22fbbd4615de651eb20965b7733a0e2f1
KyeCook <acfanatics@hotmail.com>
2016-03-21 21:55:00 +1000

    Added to pre-existing code... still not in working condition

---
+++ passwordChecker.py
@@ -0,0 +1,36 @@
+"""
+Pass Word validator
+"""
+SPECIALS = "!@#$%^&*()_-­‐=+`~,./o'[]\<>?O{}|"
+LOWEST_LENGTH = 5
+HIGHEST_LENGTH = 15
+
+lower_case = 0
+upper_case = 0
+special_characters = 0
+
+user_password = input("Please enter a valid input\n Your Password must be between 5 and 15 characters, and contain: \n\
+\t 1 or more uppercase characters\n\t 1 or more lowercase characters\n\t one or more numbers\n\
+\t and 1 or more special characters:  !@#$%^&*()_-­‐=+`~,./o'[]\<>?O|\n")
+
+for c in user_password:
+    if LOWEST_LENGTH < len(user_password) < HIGHEST_LENGTH:
+        if c.islower():
+            lower_case += 1
+        elif lower_case < 1:
+            print("Invalid Password")
+        elif c.isupper():
+            upper_case += 1
+        elif upper_case < 1:
+            print("Invalid Password")
+        elif SPECIALS in user_password:
+            special_characters += 1
+        elif special_characters < 1:
+            print("Invalid Password")
+    print("Invalid Password")
+print("Your", len(user_password), "character password is valid:", user_password)
+
+
+# while LOWEST_LENGTH < len(user_password) < HIGHEST_LENGTH:
+#
+# print("Your" + len(user_password) + "character password is valid:", user_password )
