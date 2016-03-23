4c525c5f3bf1edd21eaa923f7a4a970d42d66b77
kye cook <acfanatics@hotmail.com>
2016-03-23 13:55:25 +1000

    Attempted to break program into Funtions

--- ascii.py
+++ ascii.py
@@ -1,6 +1,38 @@
-lower = 10
-upper = 100
+"""
+Initial Program
+"""
+# lower = 10
+# upper = 100
+#
+# int(input("Enter a number ( {} - {}): ".format(lower,upper)))
+# for i in range(10,  120,  11):    #  make  sure  we  have  integers  of  different  'length'
+#     print("{}  {}".format(i,  chr(i)))
+# ---- Program With Functions ----
 
-input("Enter a number ( {} - {}): ".format(lower,upper))
-for i in range(10,  120,  11):    #  make  sure  we  have  integers  of  different  'length'
-    print("{}  {}".format(i,  chr(i)))
+
+def main():
+    lower = 10
+    upper = 100
+
+    inputted_number = get_number(10, 100)
+    print(inputted_number)
+
+    # for i in range(10,  120,  11):    #  make  sure  we  have  integers  of  different  'length'
+    #     print("{}  {}".format(i,  chr(i)))
+
+
+def get_number(lower, upper):
+
+
+    user_number = input("Enter a number ( {} - {}): ".format(lower, upper))
+    while user_number.isnumeric() != True or int(user_number) < lower or int(user_number) > upper:
+        print("Please Enter a Valid Number!")
+        user_number = input("Enter a number ( {} - {}): ".format(lower, upper))
+    return int(user_number)
+
+
+def print_ascii():
+    for i in range(10,  120,  11):    #  make  sure  we  have  integers  of  different  'length'
+        print("{}  {}".format(i,  chr(i)))
+
+main()
